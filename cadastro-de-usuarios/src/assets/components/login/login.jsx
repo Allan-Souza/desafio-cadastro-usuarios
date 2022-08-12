3a549536c39f144b27c69ec2d65b4672b82cc83d
Allan Souza <allanzissimo@gmail.com>
2022-08-12 17:52:17 -0300

    criação da tela de login

--- cadastro-de-usuarios/src/assets/components/login/login.jsx
+++ cadastro-de-usuarios/src/assets/components/login/login.jsx
@@ -1,10 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './login.css'
 
 export const Login = () => {
     return(
-        <div>
-           <h1>Página de ligin</h1>
-        </div>
+        <section id='page'>
+            <main id='main-content'>
+                <div className='container bg-light p-5 w-50 rounded mb-5' id='card-login'>
+                    <h1 className='text-center'>Login</h1> <br />
+                    <form action="#">
+                        <label htmlFor="#" className="col-sm-2 col-form-label">E-mail: </label>
+                        <input type='text' placeholder='Digite seu Nome' className="form-control"/> <br />
+                        <label htmlFor="#" className="col-sm-2 col-form-label">Senha: </label>
+                        <input type="password" placeholder='Digite sua senha' className="form-control"/>
+                        <br />
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-dark mb-3" id='botao'>Entrar</button>
+                        </div>
+                    </form>              
+                </div>
+            </main>
+        </section>       
     )
 }
 
