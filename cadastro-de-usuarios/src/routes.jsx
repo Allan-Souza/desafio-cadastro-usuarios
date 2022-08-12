20f6e9ab0cfbc775ac4bfc6ce1e86d0e09d8e5c4
Allan Souza <allanzissimo@gmail.com>
2022-08-11 21:57:10 -0300

    importação das bibliotecas

--- cadastro-de-usuarios/src/routes.jsx
+++ cadastro-de-usuarios/src/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import { Login }  from './assets/components/login/login'
 import { Cadastro }  from './assets/components/cadastro/cadastro';
 
