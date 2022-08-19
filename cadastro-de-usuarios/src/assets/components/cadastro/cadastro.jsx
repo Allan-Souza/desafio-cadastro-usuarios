92c97bb4f55c7c0b166090d99ca30a82c4ef30cb
Allan <allanzissimo@gmail.com>
2022-08-19 19:25:32 -0300

    criação da tela de cadastro

--- cadastro-de-usuarios/src/assets/components/cadastro/cadastro.jsx
+++ cadastro-de-usuarios/src/assets/components/cadastro/cadastro.jsx
@@ -2,8 +2,31 @@ import './cadastro.css'
 
 export const Cadastro = () => {
     return(
-        <div>
-            <h1>Cadastro</h1>
-        </div>
+        <section id='page-cadastro'>
+            <main id='main-content'>
+                <div className='container bg-light p-5 w-50 rounded mb-5' id='card-login'>
+                    <h1 className='text-center'>Cadastro</h1> <br />
+                    <form action="#">
+                        <label htmlFor="#" className="col-sm-6 col-form-label">Nome: </label>
+                        <input type='text' placeholder='Digite seu nome' className="form-control"/> <br />
+                        <label htmlFor="#" className="col-sm-6 col-form-label">E-mail: </label>
+                        <input type='text' placeholder='Digite seu e-mail' className="form-control"/> <br />
+                        <label htmlFor="#" className="col-sm-6 col-form-label">CPF: </label>
+                        <input type='text' placeholder='Digite seu CPF' className="form-control"/> <br />
+                        <label htmlFor="#" className="col-sm-6 col-form-label">PIS: </label>
+                        <input type='text' placeholder='Digite seu PIS' className="form-control"/> <br />
+                        <label htmlFor="#" className="col-sm-6 col-form-label">Senha: </label>
+                        <input type="password" placeholder='Digite sua senha' className="form-control"/> <br />
+                        <label htmlFor="#" className="col-sm-6 col-form-label">Confirmar senha: </label>
+                        <input type="password" placeholder='Digite novamente sua senha' className="form-control"/>
+                        <br />
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-dark mb-3" id='botao'>Cadastrar</button>
+                        </div>
+                        <p className='text-center' id='link'>Já possui cadastro? <a href="/login">Clique aqui</a></p>
+                    </form>              
+                </div>
+            </main>
+        </section>       
     )
 }
--- cadastro-de-usuarios/src/assets/components/login/login.jsx
+++ cadastro-de-usuarios/src/assets/components/login/login.jsx
@@ -3,19 +3,20 @@ import './login.css'
 
 export const Login = () => {
     return(
-        <section id='page'>
+        <section id='page-login'>
             <main id='main-content'>
                 <div className='container bg-light p-5 w-50 rounded mb-5' id='card-login'>
                     <h1 className='text-center'>Login</h1> <br />
                     <form action="#">
-                        <label htmlFor="#" className="col-sm-2 col-form-label">E-mail: </label>
-                        <input type='text' placeholder='Digite seu Nome' className="form-control"/> <br />
-                        <label htmlFor="#" className="col-sm-2 col-form-label">Senha: </label>
+                        <label htmlFor="#" className="col-sm-6 col-form-label">E-mail: </label>
+                        <input type='text' placeholder='Digite seu e-mail' className="form-control"/> <br />
+                        <label htmlFor="#" className="col-sm-6 col-form-label">Senha: </label>
                         <input type="password" placeholder='Digite sua senha' className="form-control"/>
                         <br />
                         <div class="text-center">
                             <button type="submit" class="btn btn-dark mb-3" id='botao'>Entrar</button>
                         </div>
+                        <p className='text-center' id='link'>Não possui cadastro? <a href="/cadastro">Clique aqui</a></p>
                     </form>              
                 </div>
             </main>
